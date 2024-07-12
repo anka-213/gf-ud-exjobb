19d0f5d17a01f08b8a1c5118637343f0ad7e1968
Andreas Källberg <anka.213@gmail.com>
2024-07-12 21:12:28 +0200

    Add most necessary latex packages to flake

--- flake.nix
+++ flake.nix
@@ -9,16 +9,152 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-medium latex-bin latexmk
-            chngcntr hyperref pdftexcmds infwarerr kvoptions parskip
-            etoolbox pgf epstopdf-pkg todonotes metafont
-            moreverb amsmath subfig psnfss babel-english
-            caption minted chemfig geometry eso-pic
-            float datetime2 microtype biblatex titlesec
-            fancyhdr fvextra catchfile xstring lineno framed
-            fancyvrb upquote simplekv tracklang biblatex-ieee
+          inherit (pkgs.texlive)
+
+            # scheme-medium latex-bin latexmk
+            # chngcntr hyperref pdftexcmds infwarerr kvoptions parskip
+            # etoolbox pgf epstopdf-pkg todonotes metafont
+            # moreverb amsmath subfig psnfss babel-english
+            # caption minted chemfig geometry eso-pic
+            # float datetime2 microtype biblatex titlesec
+            # fancyhdr fvextra catchfile xstring lineno framed
+            # fancyvrb upquote simplekv tracklang biblatex-ieee
+            # helvetic
+            # import csquotes glossaries acronym
+            # graphviz
+            # bigfoot
+            # xfor mfirstuc
+            # datatool
+            # tikz-dependency
+
+            scheme-small
+            latexmk
             helvetic
-            import csquotes glossaries acronym
+            biblatex-ieee
+            biber
+
+            # Generated using nix-index on sty list from \listfiles
+            acronym
+            amsfonts
+            amsmath
+            auxhook
+            babel
+            biblatex
+            bigfoot
+            bigintcalc
+            bitset
+            caption
+            catchfile
+            # chemfig
+            csquotes
+            datatool
+            datetime2
+            environ
+            epstopdf-pkg
+            eso-pic
+            etexcmds
+            etoolbox
+            fancyhdr
+            fancyvrb
+            float
+            fp
+            framed
+            fvextra
+            geometry
+            gettitlestring
+            glossaries
+            graphics
+            graphviz
+            hycolor
+            hyper
+            hyperref
+            ifplatform
+            iftex
+            import
+            infwarerr
+            intcalc
+            kvdefinekeys
+            kvoptions
+            kvsetkeys
+            l3kernel
+            l3packages
+            latex
+            letltxmacro
+            lineno
+            listings
+            lm
+            logreq
+            ltxcmds
+            makecell
+            mfirstuc
+            microtype
+            minted
+            moreverb
+            parskip
+            pdfescape
+            pdftexcmds
+            pgf
+            psnfss
+            refcount
+            rerunfilecheck
+            substr
+            supertabular
+            textcase
+            tikz-dependency
+            titlesec
+            todonotes
+            tools
+            tracklang
+            translator
+            trimspaces
+            uniquecounter
+            upquote
+            url
+            xcolor
+            xfor
+            xkeyval
+            xstring
+
+
+
+            # moreverb
+            # textcomp
+            # lmodern
+            # helvet
+            # fontenc
+            # babel
+            # inputenc
+            # amsmath
+            # amssymb
+            # graphviz
+            # graphicx
+            # subfig
+            # caption
+            # subcaption
+            # acronym
+            # glossaries
+            # tikz-dependency
+            # tikzmark
+            # tikz
+            # minted
+            # chemfig
+            # eso-pic
+            # float
+            # parskip
+            # datetime2
+            # microtype
+            # makecell
+            # lua-visual-debug
+            # hyperref
+            # biblatex
+            # csquotes
+            # listings
+            # titlesec
+            # fancyhdr
+            # todonotes
+
+            # \listfiles
+            # grep -o "^[^ ]*\.sty" thesis/needed_pkgs.txt | sed 's/\.sty//'
             ;
           # inherit (pkgs.texlive) scheme-medium chngcntr ;
         };
@@ -46,7 +182,7 @@
           };
         };
         devShell = pkgs.mkShell {
-          packages = [ tex pygments ];
+          packages = [ tex pygments pkgs.biber ];
         };
         defaultPackage = packages.document;
       });
